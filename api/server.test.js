444b905a379532377e7cd28d37f89e22425d4fd9
Daniel Castillo <danielcastillo89@icloud.com>
2023-08-16 16:19:41 -0500

    added test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,12 @@
 // Write your tests here
+//test("sanity", () => {
+//  expect(true).toBe(false);
+//});
+
 test("sanity", () => {
-  expect(true).toBe(false);
+  expect(1 + 2).toBe(3);
+});
+
+test("test 2", () => {
+  expect(5 + 5).toBe(10);
 });
